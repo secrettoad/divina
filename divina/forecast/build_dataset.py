108dc0a55c32732450494c105ce606023e1ec0ed
John Hurdle <johnhurdle23@gmail.com>
2021-07-23 15:37:34 -0700

    WIP: dataset

--- divina/forecast/build_dataset.py
+++ divina/forecast/build_dataset.py
@@ -101,7 +101,7 @@ def rm(f):
     raise TypeError('must be either file or directory')
 
 
-def build_dataset(tmp_dir='/tmp/data'):
+def build_dataset(tmp_dir=os.path.join(os.path.dirname(__file__), '/tmp/data')):
     if os.path.exists('./user-data.json'):
         with open('./user-data.json') as f:
             os.environ.update(json.load(f)['ENVIRONMENT'])
