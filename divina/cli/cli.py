6bfab3ec2e3a08643695a5cea59aac870eb6164e
John Hurdle <johnhurdle23@gmail.com>
2021-07-22 17:02:38 -0700

    WIP starting cli

--- divina/cli/cli.py
+++ divina/cli/cli.py
@@ -1,6 +1,6 @@
 import sys
 import click
-
+from ..forecast import vision
 
 @click.command()
 def cli():
@@ -12,3 +12,16 @@ def cli():
     run = getattr(imported_mod, '.'.join(submods))
     # assuming your pattern has a run method defined.
     run()
+
+
+@click.group()
+def divina():
+   pass
+
+
+@click.argument('divina_version')
+@click.argument('import_bucket')
+@divina.command()
+def forecast(divina_version, import_bucket):
+    vision.create_vision(divina_version=divina_version, import_bucket=import_bucket)
+
