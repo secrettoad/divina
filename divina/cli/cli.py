1208c71a57e33fc4bfff7728ff75f329b5138a7b
John Hurdle <johnhurdle23@gmail.com>
2021-07-22 17:11:07 -0700

    WIP starting cli

--- divina/cli/cli.py
+++ divina/cli/cli.py
@@ -2,17 +2,6 @@ import sys
 import click
 from ..forecast import vision
 
-@click.command()
-def cli():
-    mod = sys.argv[1]
-    submods = sys.argv[2:]
-    imported_mod = __import__('divina.{}'.format(mod), fromlist=['.'.join(submods)])
-    import pdb
-    pdb.set_trace()
-    run = getattr(imported_mod, '.'.join(submods))
-    # assuming your pattern has a run method defined.
-    run()
-
 
 @click.group()
 def divina():
