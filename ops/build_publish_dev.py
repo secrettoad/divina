110bb54ad25401f1aa54264150bd5f036e2cd8ce
John <johnhurdle23@gmail.com>
2021-08-12 14:28:31 -0700

    WIP

--- ops/build_publish_dev.py
+++ ops/build_publish_dev.py
@@ -11,7 +11,7 @@ def main():
                 'pip uninstall divina -y',
                 'git commit -am "WIP"',
                 'git push',
-                'pip install divina --no-index --find-links file:///{}'.format(pkg_dir),
+                'pip install divina --no-index --find-links file://{}'.format(pkg_dir),
                 'rm -rf .eggs']
     run_commands(commands)
 
