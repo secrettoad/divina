da6b38ca3da1d8dda643d4e5f7eeaf8ecca8f9eb
secrettoad <johnhurdle23@gmail.com>
2021-08-17 19:42:29 +0000

    WIP:Testing

--- divina/cli/cli.py
+++ divina/cli/cli.py
@@ -5,7 +5,7 @@ import boto3
 from ..train import dask_train
 from ..aws.utils import create_modelling_emr
 import s3fs
-from dask_cloudprovider.aws import EC2Cluster, FargateCluster
+from dask_cloudprovider.aws import EC2Cluster
 from dask.distributed import Client
 from dask_ml.linear_model import LinearRegression
 
@@ -29,7 +29,7 @@ def cli_train_vision(vision_definition, write_path, vision_name, commit='main', 
             dask_train(s3_fs=s3_fs, dask_client=dask_client, dask_model=dask_model, vision_definition=vision_definition,
                        divina_directory=write_path, vision_id=vision_name)
     elif not dask_address:
-        with FargateCluster(key_name=ec2_keypair_name) as cluster:
+        with EC2Cluster(key_name=ec2_keypair_name, security=False) as cluster:
             with Client(cluster) as dask_client:
                 dask_train(s3_fs=s3_fs, dask_client=dask_client, dask_model=dask_model, vision_definition=vision_definition, divina_directory=write_path, vision_id=vision_name)
     else:
