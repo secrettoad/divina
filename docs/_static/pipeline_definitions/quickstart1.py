920d2fa5bf0ed0fa151a193485c55abd01b2f644
GitHub Action <action@github.com>
2023-01-26 17:42:45 +0000

    Build docs

---
+++ docs/_static/pipeline_definitions/quickstart1.py
@@ -0,0 +1,16 @@
+from divina import Divina
+
+quickstart_pipeline_1 = Divina(
+    target="Sales",
+    causal_model_params=[{"link_function": "log"}, {"link_function": "None"}],
+    time_index="Date",
+    frequency="D",
+    drop_features=[
+        "Customers",
+        "StoreType",
+        "Assortment",
+        "Promo2SinceWeek",
+        "Promo2SinceYear",
+        "PromoInterval",
+    ],
+)
