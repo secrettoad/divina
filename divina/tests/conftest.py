b4a7dd41cc25f1687157dd1830f926fefe8caa20
John Hurdle <johnhurdle23@gmail.com>
2023-01-26 12:06:48 -0800

    add pbr

--- divina/tests/conftest.py
+++ divina/tests/conftest.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pytest
 from pandas import Timestamp
 
-from divina.divina.docs_src._static.pipeline_definitions import \
+from divina.docs_src._static.pipeline_definitions import \
     quickstart_pipelines
 from divina.divina.pipeline.model import EWMA, GLM
 from divina.divina.pipeline.pipeline import (BoostPrediction, BoostValidation,
