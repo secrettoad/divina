dffbdc97790b5507e5e39c5b2cad9388b475d44c
John Hurdle <johnhurdle23@gmail.com>
2023-01-26 09:25:35 -0800

    add pbr

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-prefect orion start --host=0.0.0.0
+# create /var/log/auth.log if not exist
+if [[ ! -f /var/log/auth.log ]]
+then
+    touch /var/log/auth.log
+fi
 
+# start ssh service
+service ssh start
+
+# sleep forever
+#sleep infinity
+
+# link auth.log to container log
+tail -f /var/log/auth.log
