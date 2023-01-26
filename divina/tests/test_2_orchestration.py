2d47d0b68f8019ee6bce3e2180d9ed05b286c8ef
John Hurdle <johnhurdle23@gmail.com>
2023-01-25 21:57:13 -0800

    tests pass

--- divina/tests/test_2_orchestration.py
+++ divina/tests/test_2_orchestration.py
@@ -14,7 +14,7 @@ def test_pipeline_fit_prefect(
     test_pipeline_name,
 ):
     test_pipeline_2.storage_options = {
-        "client_kwargs": {"endpoint_url": "http://127.0.0.1:{}".format(9000)}
+        "client_kwargs": {"endpoint_url": "http://localhost:{}".format(9000)}
     }
     test_data_path = "{}/test-data".format(test_pipeline_root)
     fs = s3fs.S3FileSystem(**test_pipeline_2.storage_options)
@@ -26,7 +26,7 @@ def test_pipeline_fit_prefect(
         test_data_path,
         storage_options={
             "client_kwargs": {
-                "endpoint_url": "http://127.0.0.1:{}".format(9000)
+                "endpoint_url": "http://localhost:{}".format(9000)
             }
         },
     )
